235a34c102ebdddd9014d97877dee226ebd2057d
herzstein-dev <g.m.schneeberger@gmail.com>
2023-05-04 14:18:09 +0200

    small cleanup

--- gym_2048/env.py
+++ gym_2048/env.py
@@ -400,8 +400,9 @@ class Base2048Env(gym.Env):
 
 
   def permutate_board(self, board, action_values):
-    # TODO rotate board in all 4 directions (3 directions since one is already given)
+    # rotate board in all 4 directions (3 directions since one is already given)
     # also flip board across x and y axis for more data
+    # in total 8 mutations of the board are returned
 
     rotation_action_mappings = {
       0: [0, 1, 2, 3],
